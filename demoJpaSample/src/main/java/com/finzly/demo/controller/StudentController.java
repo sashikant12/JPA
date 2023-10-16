3169a37894164910294d35ab6b6471885eee5e60
sashikant12 <SashikantSingh@DESKTOP-9V86R72>
2023-10-16 17:20:46 +0530

    last

--- demoJpaSample/src/main/java/com/finzly/demo/controller/StudentController.java
+++ demoJpaSample/src/main/java/com/finzly/demo/controller/StudentController.java
@@ -28,7 +28,7 @@ public class StudentController {
 		return studentService.getAllFromDatabaseJPA();
 	}
 	
-	@GetMapping("/newStudents")
+	@GetMapping("/new")
 	public String setStudentNameJPA(@RequestBody Student student) {
 		return studentService.setStudentNameJPA(student);
 	}
