7c79c933ccf65ec6d528bf1e033de49acb968542
sashikant12 <SashikantSingh@DESKTOP-9V86R72>
2023-10-26 12:20:33 +0530

    last change

--- demoJpaSample/src/main/java/com/finzly/demo/controller/StudentController.java
+++ demoJpaSample/src/main/java/com/finzly/demo/controller/StudentController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.finzly.demo.entity.Student;
@@ -22,56 +20,176 @@ public class StudentController {
 	@Autowired
 	StudentService studentService;
 
+	/**
+	 * Retrieve a list of all students from the database using JPA (Java Persistence
+	 * API).
+	 *
+	 * This method queries the database to fetch all student records and returns
+	 * them as a list.
+	 *
+	 * @return A list of Student objects representing all students in the database.
+	 * @author SashikantSingh
+	 */
 	@GetMapping("/students")
 	public List<Student> getAllFromDatabaseJPA() {
 		return studentService.getAllFromDatabaseJPA();
 	}
 
+	/**
+	 * Update a student in the database using JPA (Java Persistence API).
+	 *
+	 * This method is used to either create a new student or update an existing one
+	 * in the database.
+	 *
+	 * @param student The student object containing the data to be created or
+	 *                updated.
+	 * @return A response message indicating the success or failure of the
+	 *         operation.
+	 * @author SashikantSingh
+	 */
+
 	@GetMapping("/new")
-	public String setStudentNameJPA(@RequestBody Student student) {
-		return studentService.setStudentNameJPA(student);
+	public String setStudentJPA(@RequestBody Student student) {
+		return studentService.setStudentJPA(student);
 	}
+
+	/**
+	 * Retrieve a list of students by their name.
+	 *
+	 * This method queries the database to fetch all students with the specified
+	 * name and returns them as a list.
+	 *
+	 * @param name The name of the students to retrieve.
+	 * @return A list of Student objects with the specified name.
+	 * @author SashikantSingh
+	 */
 
 	@GetMapping("/names/{name}")
 	public List<Student> getAllByName(@PathVariable String name) {
 		return studentService.getAllByName(name);
 	}
 
+	/**
+	 * Retrieve a list of students by their city.
+	 *
+	 * This method queries the database to fetch all students with the specified
+	 * city and returns them as a list.
+	 *
+	 * @param city The city of the students to retrieve.
+	 * @return A list of Student objects with the specified city.
+	 * @author SashikantSingh
+	 */
+
 	@GetMapping("/cities/{city}")
 	public List<Student> getAllByCity(@PathVariable String city) {
 		return studentService.getAllByCity(city);
 	}
+
+	/**
+	 * Retrieve a list of students by their standards.
+	 *
+	 * This method queries the database to fetch all students with the specified
+	 * standards and returns them as a list.
+	 *
+	 * @param standards The standards of the students to retrieve.
+	 * @return A list of Student objects with the specified standards.
+	 * @author SashikantSingh
+	 */
 
 	@PostMapping("/standards/{standard}")
 	public List<Student> getAllByStandard(@PathVariable String standard) {
 		return studentService.getAllByStandard(standard);
 	}
 
+	/**
+	 * Retrieve a list of students by their school.
+	 *
+	 * This method queries the database to fetch all students with the specified
+	 * school and returns them as a list.
+	 *
+	 * @param school The school of the students to retrieve.
+	 * @return A list of Student objects with the specified school.
+	 * @author SashikantSingh
+	 */
+
 	@GetMapping("/schools/{school}")
 	public List<Student> getAllBySchool(@PathVariable String school) {
 		return studentService.getAllBySchool(school);
 	}
+
+	/**
+	 * Retrieve a list of students by their phone Number.
+	 *
+	 * This method queries the database to fetch all students with the specified
+	 * phone Number and returns them as a list.
+	 *
+	 * @param phone Numbers The phone Numbers of the students to retrieve.
+	 * @return A list of Student objects with the specified phone Numbers.
+	 * @author SashikantSingh
+	 */
 
 	@GetMapping("/phoneNumbers/{phoneno}")
 	public List<Student> getAllByPhoneno(@PathVariable String phoneno) {
 		return studentService.getAllByPhoneno(phoneno);
 	}
 
+	/**
+	 * Retrieve a list of students by their marks greater than the given marks.
+	 *
+	 * This method queries the database to fetch all students with the specified
+	 * marks greater than the given marks and returns them as a list.
+	 *
+	 * @param marks greater than the given marks of the students to retrieve.
+	 * @return A list of Student objects with the specified marks greater than the
+	 *         given marks.
+	 * @author SashikantSingh
+	 */
+
 	@GetMapping("/greaterMarks/{mark}")
 	public List<Student> getAllGreaterMarks(@PathVariable("mark") int mark) {
 		return studentService.getAllGreaterMarks(mark);
 	}
+
+	/**
+	 * Update city of the student by their id.
+	 *
+	 * This method queries the database to update the specific student with the id .
+	 *
+	 * @param school The city of the students to retrieve.
+	 * @return A message related to the update with the specified id.
+	 * @author SashikantSingh
+	 */
 
 	@PutMapping("/updates/{id}")
 	public String updateCityById(@PathVariable long id, @RequestParam String city) {
 		return studentService.updateCityById(id, city);
 	}
 
+	/**
+	 * Update full student detail by their id.
+	 *
+	 * This method queries the database to update the specific student with the id .
+	 *
+	 * @param city,name,phone number,standard of the students to retrieve.
+	 * @return A message related to the update with the specified id.
+	 * @author SashikantSingh
+	 */
+
 	@PutMapping("/updateByParams/{id}")
 	public ResponseEntity<String> updateStudentById(@PathVariable long id, @RequestParam String city,
 			@RequestParam String name, @RequestParam String phoneno, @RequestParam String standard) {
 		return studentService.updateStudentById(id, city, name, phoneno, standard);
 	}
+
+	/**
+	 * Update student by their id.
+	 *
+	 * This method queries the database to update the specific student with the id .
+	 *
+	 * @param student with respect to their jason file students to retrieve.
+	 * @return A message related to the update with the specified id.
+	 * @author SashikantSingh
+	 */
 
 	@PutMapping("/updateStudents/{id}")
 	public String updateStudent(@PathVariable long id, @RequestBody Student student) {
@@ -83,6 +201,17 @@ public class StudentController {
 		}
 	}
 
+	/**
+	 * Delete a student by their ID.
+	 *
+	 * This method handles the deletion of a student record from the database based
+	 * on the provided ID.
+	 *
+	 * @param id The unique identifier of the student to be deleted.
+	 * @return A message indicating whether the student was deleted successfully or
+	 *         not.
+	 * @author SashikantSingh
+	 */
 	@DeleteMapping("Delete/{id}")
 	public String deleteStudent(@PathVariable long id) {
 		boolean deleted = studentService.deleteStudentById(id);
--- demoJpaSample/src/main/java/com/finzly/demo/service/StudentService.java
+++ demoJpaSample/src/main/java/com/finzly/demo/service/StudentService.java
@@ -3,6 +3,8 @@ package com.finzly.demo.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,69 +18,87 @@ public class StudentService {
 
 	@Autowired
 	StudentRepository studentRepository;
+	private final Logger logger = LoggerFactory.getLogger(StudentService.class);
 
 	public List<Student> getAllFromDatabaseJPA() {
+		logger.info("get all students using JPA");
 		return studentRepository.findAll();
 	}
 
-	public String setStudentNameJPA(Student student) {
+	public String setStudentJPA(Student student) {
 		try {
+			logger.info("add students using JPA");
 			studentRepository.save(student);
 			return "Student Saved";
 		} catch (Exception e) {
+			logger.error("Student not added");
 			return "Some Error, Not Saved" + e;
 		}
 	}
 
 	public List<Student> getAllByCity(String city) {
+		logger.info("get all students by city using JPA");
 		return studentRepository.findByCity(city);
 	}
 
 	public List<Student> getAllByStandard(String standard) {
+		logger.info("get all students by standrad using JPA");
 		return studentRepository.findByStandard(standard);
 	}
 
 	public List<Student> getAllBySchool(String school) {
+		logger.info("get all students by school using JPA");
 		return studentRepository.findBySchool(school);
 	}
 
 	public List<Student> getAllByPhoneno(String phoneno) {
+		logger.info("get all students by phone number using JPA");
 		return studentRepository.findByPhoneno(phoneno);
 	}
 
 	public List<Student> getAllByName(String name) {
+		logger.info("get all student  by name using JPA");
 		return studentRepository.findByName(name);
 	}
 
 	public List<Student> getAllGreaterMarks(int mark) {
+		logger.info("get all students marks greater than marks using JPA");
 		return studentRepository.getAllGreaterMarks(mark);
 	}
 
 	public String updateCityById(long id, String city) {
+		logger.info("get student city update by using  student id " + id + " using JPA");
 		Optional<Student> s = studentRepository.findById(id);
 		if (s.isPresent()) {
+			logger.info("Student found with ID: " + id);
 			studentRepository.updateCityById(id, city);
 			return "City Updated";
 		} else {
+			logger.info("Student not found with ID: " + id);
 			return "Student not found with ID: " + id;
 		}
 	}
 
 	public ResponseEntity<String> updateStudentById(long id, String city, String name, String phoneno,
 			String standard) {
+		logger.info("get student info update by using PARAM and  student id " + id + " using JPA");
 		Optional<Student> s = studentRepository.findById(id);
 		if (s.isPresent()) {
+			logger.info("Student  found with ID: " + id);
 			studentRepository.updateStudentById(id, city, name, phoneno, standard);
 			return ResponseEntity.status(HttpStatus.OK).body("Student Updated");
 		} else {
+			logger.info("Student not found with ID: " + id);
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found with ID: " + id);
 		}
 	}
 
 	public boolean updateStudent(long id, Student updatedStudent) {
+		logger.info("get student city update by using JSON and student id " + id + " using JPA");
 		Student existingStudent = studentRepository.findById(id).orElse(null);
 
 		if (existingStudent != null) {
+			logger.info("Student found with ID: " + id);
 			// Update the student's information with the provided data
 			existingStudent.setName(updatedStudent.getName());
 			existingStudent.setPhoneno(updatedStudent.getPhoneno());
@@ -89,15 +109,17 @@ public class StudentService {
 			studentRepository.save(existingStudent); // Save the updated entity
 			return true; // Update successful
 		}
-
+		logger.info("Student not found with ID: " + id);
 		return false; // Student not found, update failed
 	}
 
 	public boolean deleteStudentById(long id) {
 		if (studentRepository.existsById(id)) {
+			logger.info("Student  found with ID: " + id);
 			studentRepository.deleteById(id); // Delete the student by id
 			return true; // Deletion successful
 		}
+		logger.info("Student not found with ID: " + id);
 		return false; // Student not found, deletion failed
 	}
 
